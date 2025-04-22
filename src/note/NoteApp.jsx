f518c0ab20f8bf6fcaf3beb60b89d0caad80bb7a
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-22 10:47:55 +0700

    feat: remove unused code

--- src/note/NoteApp.jsx
+++ src/note/NoteApp.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useImmerReducer } from "use-immer";
 import NoteForm from "./NoteForm";
 import NoteList from "./NoteList";
 
@@ -11,44 +11,30 @@ const initialNotes = [
 ];
 
 function notesReducer(notes, action) {
-  switch (action.type) {
-    case "ADD_NOTE":
-      return [
-        ...notes,
-        {
-          id: id++,
-          text: action.text,
-          done: false,
-        },
-      ];
-    case "CHANGE_NOTE":
-      return notes.map((note) =>
-        note.id == action.id
-          ? { ...note, text: action.text, done: action.done }
-          : note
-      );
-    case "DELETE_NOTE":
-      return notes.filter((note) => note.id !== action.id);
-    default:
-      return notes;
+  if (action.type === "ADD_NOTE") {
+    notes.push({
+      id: id++,
+      text: action.text,
+      done: false,
+    });
+  } else if (action.type === "CHANGE_NOTE") {
+    const index = notes.findIndex((item) => item.id == action.id);
+    notes[index].text = action.text;
+    notes[index].done = action.done;
+  } else if (action.type === "DELETE_NOTE") {
+    const index = notes.findIndex((item) => item.id == action.id);
+    notes.splice(index, 1);
   }
 }
 
 export default function NoteApp() {
-  const [notes, dispatch] = useReducer(notesReducer, initialNotes);
+  const [notes, dispatch] = useImmerReducer(notesReducer, initialNotes);
 
   function handleAddNote(text) {
     dispatch({
       type: "ADD_NOTE",
       text: text,
     });
-    // setNotes((draft) => {
-    //   draft.push({
-    //     id: id++,
-    //     text: text,
-    //     done: false,
-    //   });
-    // });
   }
 
   function handleChangeNote(note) {
@@ -56,10 +42,6 @@ export default function NoteApp() {
       type: "CHANGE_NOTE",
       ...note,
     });
-    // setNotes((draft) => {
-    //   const index = draft.findIndex((item) => item.id == note.id);
-    //   draft[index] = note;
-    // });
   }
 
   function handleDeleteNote(note) {
@@ -67,10 +49,6 @@ export default function NoteApp() {
       type: "DELETE_NOTE",
       id: note.id,
     });
-    // setNotes((draft) => {
-    //   const index = draft.findIndex((item) => item.id == note.id);
-    //   draft.splice(index, 1);
-    // });
   }
 
   return (
